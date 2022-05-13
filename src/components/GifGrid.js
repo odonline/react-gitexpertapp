c038e26827fff2fc38dbcd679fea297a1b87db01
Damian Ares <odonline@gmail.com>
2022-05-13 10:54:20 -0300

    agregar animacion a los elementos

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -13,8 +13,8 @@ export const GifGrid = ({ category }) => {
 
         <>
             <h3>{category}</h3>
-            {loading && <p>Cargando...</p>}
-            <div className='card-grid'>
+            {loading && <p className='animate__animated animate__flash'>Cargando...</p>}
+            <div className='card-grid animate__animated animate__fadeIn'>
 
                 {
                     images.map(gif =>
--- src/components/GifPic.js
+++ src/components/GifPic.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function GifPic({ gif }) {
     return (
-        <div className="card">
+        <div className="card animate__animated animate__bounce">
 
             <img src={gif.url} alt={gif.title}></img>
             <p>{gif.title}</p>
